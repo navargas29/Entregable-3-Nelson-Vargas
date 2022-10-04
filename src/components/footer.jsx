d0f1f6d3ab3efd8617abc46e1df84a8248472b2d
navargas29 <navargas291194@gmail.com>
2022-10-04 09:55:15 -0600

    se arregla bug

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,11 +5,12 @@ const Footer = () => {
     <footer>
       <a href=""></a>
       <p>Nelson Vargas</p>
-      <a href="#" className="icon" target={"_blank"}>
+      <a
+        href="https://github.com/navargas29/Entregable-3-Nelson-Vargas"
+        className="icon"
+        target={"_blank"}
+      >
         <i className="bx bxl-github"></i>
-      </a>
-      <a href="#" target={"_blank"} className="icon">
-        <i className="bx bxl-netlify"></i>
       </a>
       <a
         href="https://www.linkedin.com/in/nelson-vargas-b73487250/"
